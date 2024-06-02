f7b11a876df511df85bb669d6d2bb5b0a1ac6669
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-06-02 16:35:30 +0200

    Dodano plik requirements.txt

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,13 +1,6 @@
-# test_flask.py
-from app import app
+
+from app import hello
 import pytest
 
-@pytest.fixture
-def client():
-    """Test client for the Flask app."""
-    return app.test_client()
-
-def test_hello(client):
-    """Test if the app responds with 'Hello, World!'."""
-    response = client.get('/')
-    assert response.data == b'Hello, World!'
+def test_hello():
+    assert hello() == 'Hello, World!'
