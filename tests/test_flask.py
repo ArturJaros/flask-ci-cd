f3019aa8d4a220d01c74628825f9ed67d19fc750
Artur Jarosz <jaroszartur3@gmail.com>
2024-06-03 15:16:37 +0200

    Update test_flask.py

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,11 +1,5 @@
-from app import app
 import pytest
+from app import hello
 
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        yield client
-
-def test_hello(client):
-    rv = client.get('/')
-    assert rv.data == b'Hello, World!'
+def test_hello_world():
+    assert hello() == "Hello World!"
